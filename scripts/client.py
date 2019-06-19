a99d2380b29792e8edbb9166fa38e05d380d5f08
dohyun <dohyun@snu.ac.kr>
2019-06-19 22:25:31 +0900

    ros pub and sub (ongoing)

--- scripts/client.py
+++ scripts/client.py
@@ -11,17 +11,26 @@ def listener():
   ws_client = ros_ws.ws_client('192.168.1.3', 3000) # ip, port, name of client
   ws_client.connect()
 
+  # Subscribe
           # 1. The name of the topic server;
           # 2. The type of topic
           # 3. The name of the topic where to publish
           # 4. Frame rate: default = 0
           # 5. queue_length:  default = 0
 
-  ws_client.subscribe('/server/hello', String(), '/erver/hello')
+  ws_client.subscribe('/listener', String(), '/listener')
+  
+  # Publish
+  msg = Twist()
+  msg.linear.x = 999
+          # 1. the name of the topic on server where to publish; 
+          # 2. the message
+  ws_client.publish("/cmd_vel",msg)
   rospy.spin()
 
 if __name__=="__main__":
   try:
     listener()
   except:
+    print("Exception!");
     pass
